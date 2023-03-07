47f87ac24d2255588c0dee5e6d75dc41d4ac88e8
Huzayfah Patel <huzayfah.patel@outlook.com>
2023-03-07 12:02:46 +0000

    added stying to submit gif button, changed test gifs

--- src/App.js
+++ src/App.js
@@ -3,10 +3,10 @@ import twitterLogo from "./assets/twitter-logo.svg";
 import "./App.css";
 
 const TEST_GIFS = [
-  "https://i.giphy.com/media/eIG0HfouRQJQr1wBzz/giphy.webp",
-  "https://media3.giphy.com/media/L71a8LW2UrKwPaWNYM/giphy.gif?cid=ecf05e47rr9qizx2msjucl1xyvuu47d7kf25tqt2lvo024uo&rid=giphy.gif&ct=g",
-  "https://media4.giphy.com/media/AeFmQjHMtEySooOc8K/giphy.gif?cid=ecf05e47qdzhdma2y3ugn32lkgi972z9mpfzocjj6z1ro4ec&rid=giphy.gif&ct=g",
-  "https://i.giphy.com/media/PAqjdPkJLDsmBRSYUp/giphy.webp",
+  "https://media.tenor.com/lGay2n_ZrJkAAAAM/architecture-arhitktgif.gif",
+  "https://archeyes.com/wp-content/uploads/2016/04/deepblue_letters_0001_D.gif",
+  "https://i.pinimg.com/originals/81/24/97/812497327dd0042d760a79a31e182e27.gif",
+  "https://media.wired.com/photos/59327e555c4fbd732b5531c0/master/w_1600%2Cc_limit/deepblue_letters_0003_E2.gif",
 ];
 
 const TWITTER_HANDLE = "Huzayfah___";
